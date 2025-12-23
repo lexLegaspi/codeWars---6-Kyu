9f90301640ea910b9ff23e03c9dbcf6b8b1b6ff4
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2025-12-24 00:17:37 +0900

    Create groupedByCommas.js

---
+++ groupedByCommas.js
@@ -0,0 +1,27 @@
+// Finish the solution so that it takes an input n (integer) and returns a string that is the decimal representation of the number grouped by commas after every 3 digits.
+
+// Assume: 0 <= n <= 2147483647
+
+// Examples
+//        1  ->           "1"
+//       10  ->          "10"
+//      100  ->         "100"
+//     1000  ->       "1,000"
+//    10000  ->      "10,000"
+//   100000  ->     "100,000"
+//  1000000  ->   "1,000,000"
+// 35235235  ->  "35,235,235"
+
+// My Solution:
+
+function groupByCommas(n) {
+  let reversed = n.toString().split('').reverse()
+  let result = ''
+  for (let i=0;i<reversed.length ;i++) {
+    if(i%3 === 0 && i !== 0){
+      result += ','
+    }
+    result += reversed[i]
+  }
+  return(result.split('').reverse().join(''))
+}
