2d559295e52a8799cc08999a3b1545c2f944dd5f
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-01-14 00:08:17 +0900

    Create NASACountdown.js

---
+++ NASACountdown.js
@@ -0,0 +1,11 @@
+// You have the job to implement a countdown function that will be used for the next NASA spatial mission. The function takes a duration in milliseconds and return a string in countdown format.
+
+// If it's counting down, the first character should be '-', if it's counting up it should be '+'. Then it should return the number of: hours (min 2 units), minutes (2 units) and seconds (2 units).
+
+// countdown(-154800000)  // return  '-43:00:00'
+// countdown(0)           // return  '+00:00:00'
+// countdown(61000)       // return  '+00:01:01'
+// countdown(360000000)   // return '+100:00:00'
+
+// My Solution:
+
