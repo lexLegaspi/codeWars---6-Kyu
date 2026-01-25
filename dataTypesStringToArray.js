b0aad9fc4d99f6efc108ffdac8a8c18b9555fe19
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-01-26 00:55:33 +0900

    Update dataTypesStringToArray.js

--- dataTypesStringToArray.js
+++ dataTypesStringToArray.js
@@ -9,3 +9,53 @@
 // "truestring1" // [boolean, string, number]
 
 // My Solution:
+
+function dataTypes(string) {
+  let array = []
+  let newToken = true
+
+  for (let i = 0; i < string.length; i++) {
+
+    if (string[i] === ' ') {
+      newToken = true
+      continue
+    }
+
+    if (
+      newToken &&
+      string.slice(i, i + 4).toLowerCase() !== 'true' &&
+      string.slice(i, i + 5).toLowerCase() !== 'false'
+    ) {
+      if (Number.isInteger(+string[i])) {
+        array.push('number')
+      } else {
+        array.push('string')
+      }
+      newToken = false
+
+    } else if (string.slice(i, i + 4).toLowerCase() === 'true') {
+      array.push('boolean')
+      i += 3
+      newToken = false
+
+    } else if (string.slice(i, i + 5).toLowerCase() === 'false') {
+      array.push('boolean')
+      i += 4
+      newToken = false
+
+    } else {
+      if (Number.isInteger(+string[i])) {
+        if (newToken || array[array.length - 1] !== 'number') {
+          array.push('number')
+        }
+      } else {
+        if (newToken || array[array.length - 1] !== 'string') {
+          array.push('string')
+        }
+      }
+      newToken = false
+    }
+  }
+
+  return array
+}
