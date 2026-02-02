285ecc7ac684917015dda462a53eeae7b07985de
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-02-02 23:59:54 +0900

    Update reserveTablesInTheRestaurant.js

--- reserveTablesInTheRestaurant.js
+++ reserveTablesInTheRestaurant.js
@@ -27,3 +27,71 @@
 // If all bookings are accepted, the second element is just an empty string.
 
 // My Solution:
+
+function reserve(bookings) {
+  var forTwo = { name: "table for two", seats : 2, num: 4};
+	var forThree = { name: "table for three", seats : 3, num: 2};
+	var forFour = { name: "table for four", seats : 4, num: 2};
+	var forSix = { name: "table for six", seats : 6, num: 1};
+	var forEight = { name: "table for eight", seats : 8, num: 1};
+///start coding here:
+  let booked = []
+  let notBooked = 'Bookings at the following indexes were not accepted: '
+  bookings.forEach((x, i) => {
+    
+    
+    if ((x == 2 || x == 1) && forTwo.num !== 0){
+      booked.push(forTwo.name)
+      forTwo.num --
+    }else if((x == 3 || x == 2) && forThree.num !== 0){
+      booked.push(forThree.name)
+      forThree.num --
+    }else if((x == 4 || x == 3) && forFour.num !== 0){
+      booked.push(forFour.name)
+      forFour.num --
+    }else if((x == 6 || x == 5) && forSix.num !== 0){
+      booked.push(forSix.name)
+      forSix.num --
+    }else if((x == 8 || x == 7) && forEight.num !== 0){
+      booked.push(forEight.name)
+      forEight.num --
+    }else {
+      notBooked += `${i}, `
+    }
+  })
+  return [booked, notBooked.slice(0, -2)] //aaasdasd
+  
+}function reserve(bookings) {
+  var forTwo = { name: "table for two", seats : 2, num: 4};
+	var forThree = { name: "table for three", seats : 3, num: 2};
+	var forFour = { name: "table for four", seats : 4, num: 2};
+	var forSix = { name: "table for six", seats : 6, num: 1};
+	var forEight = { name: "table for eight", seats : 8, num: 1};
+///start coding here:
+  let booked = []
+  let notBooked = 'Bookings at the following indexes were not accepted: '
+  bookings.forEach((x, i) => {
+    
+    
+    if ((x == 2 || x == 1) && forTwo.num !== 0){
+      booked.push(forTwo.name)
+      forTwo.num --
+    }else if((x == 3 || x == 2) && forThree.num !== 0){
+      booked.push(forThree.name)
+      forThree.num --
+    }else if((x == 4 || x == 3) && forFour.num !== 0){
+      booked.push(forFour.name)
+      forFour.num --
+    }else if((x == 6 || x == 5) && forSix.num !== 0){
+      booked.push(forSix.name)
+      forSix.num --
+    }else if((x == 8 || x == 7) && forEight.num !== 0){
+      booked.push(forEight.name)
+      forEight.num --
+    }else {
+      notBooked += `${i}, `
+    }
+  })
+  return [booked, notBooked.slice(0, -2)] || [[],''] //aaasdasd
+  
+}
