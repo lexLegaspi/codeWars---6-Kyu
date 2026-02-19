208be16b7a3629104aa42a6af8f6e568457c1a53
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-02-19 22:53:09 +0900

    Update twistedSum.js

--- twistedSum.js
+++ twistedSum.js
@@ -12,3 +12,19 @@
 
 // My Solution:
 
+function twistedSum(n) {
+    let sum = 0
+    
+    for (let i = 1; i<=n;i++) {
+      let num = i
+      
+      while (num>0){
+        sum += num % 10;
+        num = Math.trunc(num / 10);
+      }
+    }
+  
+  return sum
+}
+
+console.log(twistedSum(11))
