f7774534000d1e73113486680a6fa7f7f0f8df8f
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-02-23 23:25:35 +0900

    Update insideOutStrings.js

--- insideOutStrings.js
+++ insideOutStrings.js
@@ -9,3 +9,38 @@
 // Words will be separated by exactly one space and there will be no leading or trailing spaces.
 
 // My solution:
+
+function insideOut(x) {
+  let words = x.split(" ");
+  let result = [];
+  for (let word of words) {
+    if (word.length < 4) {
+      result.push(word);
+    } else {
+      if ((word.length & 1) == 0) {
+        let left = "";
+        let right = "";
+        for (let i = 0; i < Math.floor(word.length / 2); i++) {
+          right += word[i + word.length / 2];
+          left += word[i];
+        }
+        let rightIO = right.split("").reverse().join("");
+        let leftIO = left.split("").reverse().join("");
+        result.push(leftIO + rightIO);
+      } else {
+        // odd length
+        let left = "";
+        let right = "";
+        let middleIndex = Math.floor(word.length / 2);
+        for (let i = 0; i < middleIndex; i++) {
+          left += word[i];
+          right += word[i + middleIndex + 1];
+        }
+        let leftIO = left.split("").reverse().join("");
+        let rightIO = right.split("").reverse().join("");
+        result.push(leftIO + word[middleIndex] + rightIO);
+      }
+    }
+  }
+  return result.join(" ");
+}
