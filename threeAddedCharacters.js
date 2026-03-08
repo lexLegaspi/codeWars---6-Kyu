645528ae7afb061b84d1bbf48a3ad79ba387626d
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-03-08 23:10:13 +0900

    Create threeAddedCharacters.js

---
+++ threeAddedCharacters.js
@@ -0,0 +1,20 @@
+// Given two strings, the first being a random string and the second being the same as the first, but with three added characters somewhere in the string (three same characters),
+
+// Write a function that returns the added character
+
+// You can assume that string2 will aways be larger than string1, and there will always be three added characters in string2.
+
+// Examples
+// string1 = "hello"
+// string2 = "aaahello"
+// => 'a'
+
+// string1 = "abcde"
+// string2 = "2db2a2ec"
+// => '2'
+
+// string1 = "aabbcc"
+// string2 = "aacccbbcc"
+// => 'c'
+
+// My Solution:
