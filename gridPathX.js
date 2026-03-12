14b979b643d278ff200759af2182026c7718b1a9
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-03-12 22:57:27 +0900

    Update gridPathX.js

--- gridPathX.js
+++ gridPathX.js
@@ -11,7 +11,7 @@
 //   .....
 //   .....
 //   ..x..
-//   xx..x 
+//   xx..x
 // Output: 2
 // As you can see, in the last row (xx..x) of the grid, you only need to replace two periods with x's to get a straight n-length path of x's in the grid. Any other row, column, or diagonal will require more replacements than 2. So 2 is the minimum number of periods to be replaced with x's.
 
@@ -19,3 +19,31 @@
 
 // My Solution:
 
+function minXPath(grid) {
+  let n = grid.split("\n").length;
+  let map = grid.split("\n").map((row) => row.split(""));
+  let max = 0;
+
+  for (let x = 0; x < map.length; x++) {
+    let current = map[x].filter((x) => x === "x").length;
+    if (current > max) max = current;
+    current = 0;
+    for (let y = 0; y < map[x].length; y++) {
+      if (map[y][x] == "x") current++;
+    }
+    if (current > max) max = current;
+  }
+
+  let current = 0;
+  for (let i = 0; i < n; i++) {
+    if (map[i][i] === "x") current++;
+  }
+  if (current > max) max = current;
+
+  current = 0;
+  for (let i = 0; i < n; i++) {
+    if (map[i][n - 1 - i] === "x") current++;
+  }
+  if (current > max) max = current;
+  return n - max;
+}
