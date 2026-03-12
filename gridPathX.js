1f86deeb24b87cac61e4bf8a36262744d5ceb3e4
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-03-12 22:56:01 +0900

    Create gridPathX.js

---
+++ gridPathX.js
@@ -0,0 +1,21 @@
+// Given an n by n grid filled with lowercase x's and periods (.), return the minimum number of periods to be replaced with x's in order to form a continuous, straight, n-length path of x's, from either: side to side, top to bottom, or diagonally.
+
+// Input and Output:
+// Input: a string formatted as an n by n grid filled with randomly placed lowercase x's and periods. Each line of the grid is separated from the next by a linebreak.
+
+// Output: a number between 0 and n, inclusive. The return number represents the minimum number of periods to be replaced with x's to obtain a straight n-length path of x's.
+
+// Example:
+// Input:
+//   ..x..
+//   .....
+//   .....
+//   ..x..
+//   xx..x 
+// Output: 2
+// As you can see, in the last row (xx..x) of the grid, you only need to replace two periods with x's to get a straight n-length path of x's in the grid. Any other row, column, or diagonal will require more replacements than 2. So 2 is the minimum number of periods to be replaced with x's.
+
+// For more examples check out the Example Test Cases.
+
+// My Solution:
+
