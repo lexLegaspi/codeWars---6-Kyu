7c29a72dfcaabb6ebbf2a31f528ad801fbbe6a36
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-03-13 23:05:33 +0900

    Update separateTheWheatFromTheChaff.js

--- separateTheWheatFromTheChaff.js
+++ separateTheWheatFromTheChaff.js
@@ -12,7 +12,7 @@
 // Repetition of numbers in the input sequence could occur , so duplications are included when separating.
 // If a misplaced positive number is found in the front part of the sequence, replace it with the last misplaced negative number (the one found near the end of the input). The second misplaced positive number should be swapped with the second last misplaced negative number. Negative numbers found at the head (begining) of the sequence , should be kept in place .
 // Input >> Output Examples:
-// wheatFromChaff ({7, -8, 1 ,-2}) ==> return ({-2, -8, 1, 7}) 
+// wheatFromChaff ({7, -8, 1 ,-2}) ==> return ({-2, -8, 1, 7})
 // Explanation:
 // Since 7  is a positive number , it should not be located at the beginnig so it needs to be swapped with the last negative number -2.
 // wheatFromChaff ({-31, -5, 11 , -42, -22, -46, -4, -28 }) ==> return ({-31, -5,- 28, -42, -22, -46 , -4, 11})
@@ -32,3 +32,19 @@
 
 // My Solution:
 
+function wheatFromChaff(values) {
+  // Your Code is Here .. Enjoy !!
+  let left = 0;
+  let right = values.length - 1;
+
+  while (left < right) {
+    while (values[left] < 0 && left < right) left++;
+    while (values[right] > 0 && left < right) right--;
+    if (values[left] > 0 && values[right] < 0) {
+      [values[left], values[right]] = [values[right], values[left]];
+      left++;
+      right--;
+    }
+  }
+  return values;
+}
