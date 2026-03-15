38dbda1f4e405b501e80c11f1c56cfbf503a2221
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-03-16 03:24:32 +0900

    Create maximumValueInNestedArrays.js

---
+++ maximumValueInNestedArrays.js
@@ -0,0 +1,6 @@
+// Build a function findNestedMaximum that finds the maximum value in an array containing both numbers and nested arrays. The nested arrays themselves can contain both numbers and further nested arrays. Some of the arrays may be empty but at least one number will be present amongst all the arrays.
+
+// For example, findNestedMaximum([4, -3, [10], [[-5, 6], 18]]) should return 18.
+
+// My Solution:
+
