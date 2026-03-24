cb50476f870c696da6172c7c8ceb909ff882d7ba
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-03-24 22:51:27 +0900

    Update stringTransformations.js

--- stringTransformations.js
+++ stringTransformations.js
@@ -23,3 +23,42 @@
 // Input string length is 1 <= n <= 10^5, length of transformations is 0 <= n <= 10^5, in all tests is guaranteed that the string will be not longer than 10^8.
 
 // My Solution:
+
+function stringTransformation(string, transformations) {
+  // Good luck :)
+  string = string.split("");
+  let lastCase = null;
+  let reverseCount = 0;
+  let dupCount = 0;
+
+  for (let i = 0; i < transformations.length; i++) {
+    if (transformations[i] == "U" || transformations[i] == "L") {
+      lastCase = transformations[i];
+    } else if (transformations[i] == "R") {
+      reverseCount++;
+    } else if (transformations[i] == "D") {
+      dupCount++;
+    }
+  }
+
+  let isReversed = reverseCount % 2 !== 0;
+  let repeat = Math.pow(2, dupCount);
+
+  if (lastCase == "U") {
+    string = string.map((x) => x.toUpperCase());
+  }
+
+  if (lastCase == "L") {
+    string = string.map((x) => x.toLowerCase());
+  }
+
+  if (isReversed) {
+    string = string.reverse();
+  }
+
+  if (dupCount > 0) {
+    string = string.map((x) => x.repeat(repeat));
+  }
+
+  return string.join("");
+}
