c26261d581b7520b030edade2ba03ad82c4d0e5b
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-04-11 23:35:28 +0900

    Create stringIntegerGreaterThan.js

---
+++ stringIntegerGreaterThan.js
@@ -0,0 +1,5 @@
+You are to write a function, stringIntGreaterThan that must accept arguments, each being the string representation of an positive or negative integer of any length (could contain a thousand digits, or more; also, integers will not have leading zeros).
+
+The function should then return true if the first argument is greater than the second argument, false if not.
+
+My Solution: 
