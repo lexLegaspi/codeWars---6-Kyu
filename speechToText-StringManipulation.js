b214dd5d3bf9bf493f06946fa7284f562f05c83d
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-04-12 23:08:40 +0900

    Update speechToText-StringManipulation.js

--- speechToText-StringManipulation.js
+++ speechToText-StringManipulation.js
@@ -10,3 +10,37 @@
 
 // My Solution:
 
+const bot = {
+    message(prompt) {
+        prompt = prompt.split(' ')
+        if(prompt[0] == 'Add'){
+          return +prompt[1] + +prompt[3]
+        }else if(prompt[0] == 'Subtract'){
+          return +prompt[3] - +prompt[1]
+        } else {
+          let time = prompt[prompt.length - 1].toLowerCase()
+
+          if (time.endsWith('?')) {
+            time = time.slice(0, -1)
+          }
+
+          let [hour, minute] = [
+            time.split(':')[0],
+            time.split(':')[1].slice(0, -2)
+          ].map(Number)
+
+          if (hour == 12 && time.includes('am')) {
+            hour = 0
+          }
+
+          let timeInMinutes = hour * 60 + minute
+
+          if (time.includes('pm') && hour !== 12){
+            timeInMinutes += 12 * 60
+          }
+
+          if(timeInMinutes >= 6 * 60 && timeInMinutes <= 18 * 60) return 'sunny'
+          else return 'raining'
+        }
+    }
+};
