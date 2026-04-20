243c263f8f3842fd9c6e654d7e823a6b0153ab41
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-04-20 23:40:06 +0900

    Update stringAverage.js

--- stringAverage.js
+++ stringAverage.js
@@ -5,3 +5,29 @@
 // If the string is empty or includes a number greater than 9, return "n/a"
 
 // My Solution:
+
+function averageString(str) {
+  let numbers = "zeroonetwothreefourfivesixseveneightnine";
+  let map = {
+    zero:   0,
+    one:    1,
+    two:    2,
+    three:  3,
+    four:   4,
+    five:   5,
+    six:    6,
+    seven:  7,
+    eight:  8,
+    nine:   9,
+  };
+  const reverseMap = Object.fromEntries(
+    Object.entries(map).map(([key, value]) => [value, key]),
+  );
+  if (str == "") return "n/a";
+
+  str = str.split(" ");
+  if (str.some((x) => !numbers.includes(x))) return "n/a";
+  return reverseMap[
+    Math.floor(str.map((x) => map[x]).reduce((a, b) => a + b, 0) / str.length)
+  ];
+}
