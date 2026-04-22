15ee53079b789d5417df027e4ebcfb33c6972f9c
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-04-22 23:11:41 +0900

    Update jumbleWords.js

--- jumbleWords.js
+++ jumbleWords.js
@@ -9,3 +9,51 @@
 // If you solve this kata,check how many percent of changeable words you have changed.If you reach 100% you will get a reward!
 
 // My Solution:
+
+function jumble(string) {
+  let result = "";
+  let word = "";
+
+  function isAlphaNumeric(char) {
+    let code = char.charCodeAt(0);
+    return (
+      (code >= 48 && code <= 57) || // 0-9
+      (code >= 65 && code <= 90) || // A-Z
+      (code >= 97 && code <= 122)   // a-z
+    );
+  }
+
+  function shuffleMiddle(w) {
+    if (w.length <= 3) return w;
+
+    let first = w[0];
+    let last = w[w.length - 1];
+    let middle = w.slice(1, -1).split("");
+
+    for (let i = middle.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = middle[i];
+      middle[i] = middle[j];
+      middle[j] = temp;
+    }
+
+    return first + middle.join("") + last;
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+
+    if (isAlphaNumeric(char)) {
+      word += char;
+    } else {
+      result += shuffleMiddle(word);
+      word = "";
+
+      result += char;
+    }
+  }
+
+  result += shuffleMiddle(word);
+
+  return result;
+}
