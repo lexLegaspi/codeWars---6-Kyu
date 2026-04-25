011dc4de2e3c51b48ef079dc9eaaaaebdad40c43
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-04-25 22:54:42 +0900

    Create integersSumSquare.js

---
+++ integersSumSquare.js
@@ -0,0 +1,9 @@
+// Given positive integers n and m, find all integers less than n whose the square of the sum of thier digits is equal to m. If you haven't such integers return No result.
+
+// m=5, n=4    -> [2]
+// m=50, n=15  -> 'No result'
+// m=156, n=16 -> [4,13,22,31,40,103,112,121,130]
+// m=297, n=25 -> [5,14,23,32,41,50,104,113,122,131,140,203,212,221,230]
+
+// My Solution:
+
