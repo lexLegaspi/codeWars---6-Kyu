f89710f9d23e0f3b1072f42f2075ce8baef491b5
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-04-27 22:38:28 +0900

    Update onlyDuplicates.js

--- onlyDuplicates.js
+++ onlyDuplicates.js
@@ -7,3 +7,16 @@
 // output: "cceee"
 
 // My Solution:
+
+function onlyDuplicates(str) {
+  let counts = {};
+
+  for (let char of str) {
+    counts[char] = (counts[char] || 0) + 1;
+  }
+
+  return str
+    .split('')
+    .filter(char => counts[char] > 1)
+    .join('');
+}
